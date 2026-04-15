1c79900bf996d56c578adf125f23c2c6c2160543
Venesi0 <corentin.piv@gmail.com>
2026-04-15 18:19:55 +0000

    slight correction

--- scripts/up.sh
+++ scripts/up.sh
@@ -9,15 +9,3 @@ docker compose -f "$COMPOSE_FILE" up -d
 echo
 echo "[INFO] État des services :"
 docker compose -f "$COMPOSE_FILE" ps
-
-VM_IP="$(hostname -I | awk '{print $1}')"
-
-echo
-echo "[INFO] Accès à l'application"
-echo "Local :  http://localhost"
-
-if [ -n "$VM_IP" ]; then
-  echo "Réseau : http://$VM_IP"
-else
-  echo "Réseau : IP non détectée automatiquement"
-fi
